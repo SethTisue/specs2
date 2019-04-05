37b04a15da1056ef21ab5f09fa1e391356734b29
Seth Tisue <seth@tisue.net>
2019-04-06 00:08:08 +0100

    assorted bumps, preparing for Scala 2.13.0-RC1 support
    references #735

--- project/depends.scala
+++ project/depends.scala
@@ -28,11 +28,11 @@ object depends {
         scalaJSStage in Test := FastOptStage)
 
   def scalaParser = Def.setting {
-    Seq("org.scala-lang.modules" %%% "scala-parser-combinators" % "1.1.1")
+    Seq("org.scala-lang.modules" %%% "scala-parser-combinators" % "1.1.2")
   }
 
   def scalaXML = Def.setting {
-    Seq("org.scala-lang.modules" %% "scala-xml" % "1.1.1")
+    Seq("org.scala-lang.modules" %% "scala-xml" % "1.2.0")
   }
 
   def kindp(scalaVersion: String) =
